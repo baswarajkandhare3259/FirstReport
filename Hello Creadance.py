38dcc061cae69f422e446810dc5409a1f9c498ec
Baswaraj github <kandharebasav789@gmail.com>
2022-08-26 01:12:29 +0530

    Python Code

---
+++ Hello Creadance.py
@@ -0,0 +1,35 @@
+#Developer Name : Sagar     # Single line comment
+#Company : Credence
+#Created Date : 14-Jun-2022
+#Modified Date : 14-Jun-2022
+
+# ctrl + /   => comment/uncommnet 
+# ctrl + k + c  => comment
+# ctrl + k + u => uncomment
+
+# /  => topi => forward slash
+# \  => topi => backward slash 
+
+
+print("Hello Credence")     # printing message on output
+
+###write("Hello Credence")  # write is not defined, no meaning for ##
+
+# Multiline comment
+"""Developer Name : Sagar     
+Company : Credence
+Created Date : 14-Jun-2022
+Modified Date : 14-Jun-2022"""
+
+# Multiline comment
+'''Developer Name : Sagar     
+Company : Credence
+Created Date : 14-Jun-2022
+Modified Date : 14-Jun-2022'''
+
+# Developer Name : Sagar     
+# Company : Credence
+# Created Date : 14-Jun-2022
+# Modified Date : 14-Jun-2022
+
+

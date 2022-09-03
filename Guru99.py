5e2d7c11129783d77eb9d5dbe4750dfd8ed87fa4
Baswaraj github <kandharebasav789@gmail.com>
2022-09-03 22:41:05 +0530

    Guru99.py

---
+++ Guru99.py
@@ -0,0 +1,14 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# driver = webdriver.Chrome(executable_path="C:/Users/Baswaraj/AppData/Local/Programs/Python/Python310/chromedriver.exe")
+#
+# driver.get("https://clicks.aweber.com/y/ct/?l=BXO08&m=mqr21DGG7WFEjy9&b=ftyRAGqn86J.zqoUqwX_JA")
+#
+# driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[1]/td[2]/input").send_keys("mngr436047")
+#
+# driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[2]/td[2]/input").send_keys("hyvyjYj")
+#
+# driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[3]/td[2]/input[1]").click()
+
+
